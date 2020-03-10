636cf7e760706cb948f007a42894cde6c4434e37
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-10 20:42:50 +0100

    #8 update todolist model, add allowRead/allowWrite arrays

--- src/app/model/todolist.ts
+++ src/app/model/todolist.ts
@@ -2,6 +2,8 @@ import {Todo} from "./todo";
 
 export interface Todolist {
     todos : Array<Todo>;
+    allowRead : Array<string>;
+    allowWrite : Array<string>;
     id?: string;
     name : string;
     owner : string;
