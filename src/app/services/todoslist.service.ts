f5fe7525b1a5e0a4e92d11f963cc4cc4081776cb
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-10 16:45:57 +0100

    #8: retrieve user using fireAuth and filter todolists based on email (change to uuid ?)

--- src/app/services/todoslist.service.ts
+++ src/app/services/todoslist.service.ts
@@ -5,6 +5,8 @@ import {flatMap, map} from 'rxjs/operators';
 import {Todolist} from "../model/todolist";
 import {Todo} from "../model/todo";
 import {toTitleCase} from "codelyzer/util/utils";
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
     providedIn: 'root'
@@ -15,12 +17,26 @@ export class TodoslistService {
     private todolists: Observable<Array<Todolist>>;
     private items: Array<Todo>;
 
-    constructor(private db: AngularFirestore) {
+    constructor(private db: AngularFirestore, public afAuth: AngularFireAuth) {
+        const user = afAuth.auth.currentUser;
+        console.log('user : ' + user);
+        console.log('user.displayName' + user.displayName);
+        console.log('user.email' + user.email);
+        console.log('user.uid' + user.uid);
+        console.log('user.photoURL' + user.photoURL);
+
+/**
+        if (user) {
+            // User is signed in.
+        } else {
+            // No user is signed in.
+        }
+*/
         // return all list (Array of Observable Todolist containing Array of todo)
         // TODO implements user filtering
         // TODO implements shared lists
         // Load collection containing all todolist document
-        this.todolistsCollection = db.collection<Todolist>('list');
+        this.todolistsCollection = db.collection<Todolist>('list',ref => ref.where('owner', '==', user.email));
         this.todolists = this.todolistsCollection.snapshotChanges().pipe(
             map(this.convertSnapshots), // data of each todolist
             map((allTodolistDatas:Todolist[]) => allTodolistDatas.
