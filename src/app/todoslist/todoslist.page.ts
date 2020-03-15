fdbd930be9f52a6726aeab02e831c41140442546
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-15 19:43:28 +0100

    #15 update todoslist page, replace observable of all todolists to an observable of the 3 queried todolists (owner, allowR, allowW)

--- src/app/todoslist/todoslist.page.ts
+++ src/app/todoslist/todoslist.page.ts
@@ -10,13 +10,24 @@ import {Todolist} from "../model/todolist";
   styleUrls: ['./todoslist.page.scss'],
 })
 export class TodoslistPage implements OnInit {
-  private todolists$: Observable<Array<Todolist>>;
+  private todolists$: Observable<Array<Array<Todolist>>>;
+  private ownerTodolist:Array<Todolist>;
+  private allowReadTodolist:Array<Todolist>;
+  private allowWriteTodolist:Array<Todolist>;
+
 
   constructor(private listService: TodoslistService) {}
 
+  /**
+   * Get an observable and set/update the 3 todolists (owner, allowR, allowW)
+   */
   ngOnInit(): void {
     this.todolists$ = this.listService.get();
-    // Display data in html file
+    this.todolists$.subscribe(todolists => {
+    this.ownerTodolist = todolists[0];
+    this.allowReadTodolist = todolists[1];
+    this.allowWriteTodolist = todolists[2];
+  })
   }  
 /**
   TODO MUST BE DONE
