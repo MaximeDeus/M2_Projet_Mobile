5d86dc05329f1e19e17531c1a2458738cad38b6b
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-19 19:29:18 +0100

    #16 feat: create user service, for only for retrieving authenticated user

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {User} from "firebase";
+import {Observable} from "rxjs";
+import {Todolist} from "../model/todolist";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private user:User
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.auth.currentUser;
+  }
+
+  get(): User {
+    return this.user;
+  }
+}
