f1ab7dfeee56b971d2e928bad49c23d7d774d983
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-19 21:05:34 +0100

    #8 feat: add redirection to todoslist page if user had been successfully authenticated

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -13,9 +14,15 @@ export class AuthPage {
   password:string = null;
 
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,private router:Router,) { }
   login() {
-    this.userService.login(this.email,this.password);
+    this.userService.login(this.email,this.password).then(() => {
+        // redirect if success
+        this.router.navigate(['']);
+    }).catch(err => {
+      // TODO handle error (display error message)
+      console.log ("err: ", JSON.stringify(err));
+    });
   }
   logout() {
     this.userService.logout();
