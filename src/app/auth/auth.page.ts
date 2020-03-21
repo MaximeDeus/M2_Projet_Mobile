3fcc53ad258794bbe8b95a5da3e006793f1fe6ee
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-21 15:35:15 +0100

    #6 feat: handle error when something went wrong during authentication

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -12,15 +12,20 @@ import {Router} from "@angular/router";
 export class AuthPage {
   email: string = null;
   password:string = null;
+  error:Error = null;
 
 
   constructor(private userService: UserService,private router:Router,) { }
   login() {
     this.userService.login(this.email,this.password).then(() => {
         // redirect if success
+        this.error = null;
         this.router.navigate(['']);
-    }).catch(err => {
-      // TODO handle error (display error message)
+    }).catch(err => { // Display message if any issue during authentication
+      this.error = err;
+      if (this.error.message === 'There is no user record corresponding to this identifier. The user may have been deleted.') {
+        this.error.message = 'Invalid credentials. Please try again.'
+      }
       console.log ("err: ", JSON.stringify(err));
     });
   }
