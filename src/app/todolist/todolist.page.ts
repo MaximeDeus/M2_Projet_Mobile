27230cc578aef1b791b7b2f61732209fd2e03e3e
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-03-22 01:56:04 +0100

    #17 feat: Create todolist service, for now just displaying name of todolist and todo. Algorithm for retrieving data must be improved (Observe only this todolist)

---
+++ src/app/todolist/todolist.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Todolist} from "../model/todolist";
+import {ActivatedRoute} from "@angular/router";
+import {TodoslistService} from "../services/todoslist.service";
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.page.html',
+  styleUrls: ['./todolist.page.scss'],
+})
+export class TodolistPage implements OnInit {
+
+  private id: string;
+  private todolist:Todolist;
+
+  constructor(private todolistService: TodoslistService , private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    // TODO this.todolist = this.todolistService.getTodolist(this.id);
+    this.todolistService.get().subscribe(todolists => {
+      let mergedTodolists: Array<Todolist> = [].concat.apply([], todolists);
+      // let unique = [...new Set(names)];
+      console.log ('todolists :' , JSON.stringify(todolists));
+      console.log ('mergedTodolists :' , JSON.stringify(mergedTodolists));
+      console.log ('this.todolist avant :' , JSON.stringify(this.todolist));
+      this.todolist = mergedTodolists.find(todolist => todolist.id === this.id);
+      console.log ('this.todolist après :' , JSON.stringify(this.todolist));
+    })
+
+    // this.todolist = {allowRead: undefined, allowWrite: undefined, id: "", name: "", owner: "", todos: undefined};
+    }
+}
