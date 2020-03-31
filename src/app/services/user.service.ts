fff0ddc66a63087529c7039d96c5b8c4a3e79353
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-04-01 00:29:22 +0200

    #16 enhancement: update user service, add signUp and edit methods, must clean commentaries

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/auth";
 import {User} from "firebase";
 import {Observable} from "rxjs";
 import {Todolist} from "../model/todolist";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
-  private user:User
-  constructor(public afAuth: AngularFireAuth) {
-    this.user = afAuth.auth.currentUser;
-  }
+    constructor(public afAuth: AngularFireAuth) {
+    }
 
-  get(): User {
-    return this.user;
-  }
+    get(): User {
+        return this.afAuth.auth.currentUser;
+    }
 
-  login(email,password) {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
-  }
-  logout() {
-    this.afAuth.auth.signOut();
-  }
+    login(email, password) {
+        return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    logout() {
+        return this.afAuth.auth.signOut();
+    }
+
+    signUp(email, password) {
+        return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    editUserName(displayName) {
+        console.log('displayName : ', displayName);
+        return this.afAuth.auth.currentUser.updateProfile({displayName: displayName});
+    }
+
+    editUser(userCredential, displayName, email, photoURL, password) {
+        // return userCredential.user.updateEmail('newyou@domain.com');
+        // this.afAuth.auth.currentUser.updateProfile({displayName:displayName});
+        return Promise.all([
+            this.afAuth.auth.currentUser.updateProfile({displayName: displayName}),
+            this.afAuth.auth.currentUser.updateEmail(email),
+            this.afAuth.auth.currentUser.updatePassword(password)
+        ]);
+    }
 }
+
+/**
+ let updateOperations : Array<Promise<void>> = [];
+ updateOperations.push(this.afAuth.auth.currentUser.updateProfile({displayName:displayName}));
+ updateOperations.push(this.afAuth.auth.currentUser.updateEmail(email));
+ updateOperations.push(this.afAuth.auth.currentUser.updatePassword(password));
+ return Promise.all(updateOperations);
+ }
+ }*/
