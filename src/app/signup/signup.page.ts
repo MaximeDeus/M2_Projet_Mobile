92c0e588d84e626bddfa9b081d6efa7d20efb6a2
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-04-01 00:58:31 +0200

    #10 enhancement: update signup BE, add displayName field, improve signUp method, handle error (should be improved), must clean commentaries

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,23 +1,43 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/auth";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+    selector: 'app-signup',
+    templateUrl: './signup.page.html',
+    styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage {
-  email: string = null;
-  password: string = null;
+    displayName: string = null;
+    email: string = null;
+    password: string = null;
+    error: Error = null;
 
-  constructor(public afAuth: AngularFireAuth) { }
-  signup() {
-    console.log(`email : ${this.email} \n password : ${this.password} `);
-    this.afAuth.auth.createUserWithEmailAndPassword(this.email,this.password).then((res)=> console.log(res));
-    // TODO redirect to /home with message Hello {userName}
-    // TODO (add button 'My todolists' who redirect to right todolists)
-    // TODO implements guards
-    // this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
-  }s
+    constructor(public afAuth: AngularFireAuth, public userService: UserService, private router: Router) {
+    }
 
+    signUp() {
+        console.log(`displayName : ${this.displayName} \n email : ${this.email} \n password : ${this.password} `);
+        /**Promise.all([
+         this.userService.signUp(this.email, this.password),
+         this.userService.login(this.email, this.password),
+         this.userService.editUserName(this.displayName)])
+
+         */
+        this.userService.signUp(this.email, this.password).then(() => {
+            this.userService.login(this.email, this.password).then(() => {
+                this.userService.editUserName(this.displayName).then(() => {
+                    this.error = null
+                    this.router.navigate(['']);
+                }).catch(err => {
+                    this.error = err;
+                })
+            }).catch(err => {
+                this.error = err;
+            })
+        }).catch(err => {
+            this.error = err;
+        })
+    }
 }
