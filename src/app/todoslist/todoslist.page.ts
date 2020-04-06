f44bed509698647a59e764aab70d613de3974b67
MaximeDeus <maxime.deroissart@club-internet.fr>
2020-04-06 22:37:21 +0200

    #13 update: (WIP) now get all registered users using userService, display prompt share todolist associated to users, must improve & clean code

--- src/app/todoslist/todoslist.page.ts
+++ src/app/todoslist/todoslist.page.ts
@@ -5,6 +5,7 @@ import {Todolist} from "../model/todolist";
 import {User} from "firebase";
 import {UserService} from "../services/user.service";
 import {AlertController} from "@ionic/angular";
+import {UserDB} from "../model/user";
 
 
 @Component({
@@ -20,24 +21,34 @@ export class TodoslistPage implements OnInit, OnDestroy {
     private allowReadWriteTodolist: Array<Todolist>;
     private currentUser: User;
     private subscriptionTodolists$: Subscription;
+    private onInitState = true;
+    private usersObservable: Observable<Array<UserDB>>;
+    private users: Array<UserDB>;
 
 
     constructor(private listService: TodoslistService,
                 private userService: UserService,
                 public alertCtrl: AlertController,
-                ) {}
+    ) {
+    }
 
     /**
      * Get an observable and set/update the 3 todolists (owner, allowR, allowW)
      */
     ngOnInit(): void {
         console.log("NGONINIT");
+        this.userService.init();
         this.currentUser = this.userService.get();
+        this.usersObservable = this.userService.getUsers();
+        this.usersObservable.subscribe(users => {
+            this.users = users;
+        })
+        this.listService.init();
         console.log("Current user : " + JSON.stringify(this.currentUser));
-        this.ownerTodolist = this.listService.getLatestOwnerTodolist();
-        console.log('owner todolist : ' , this.ownerTodolist);
+        // this.ownerTodolist = this.listService.getLatestOwnerTodolist();
+        console.log('owner todolist : ', this.ownerTodolist);
         this.todolists$ = this.listService.get();
-        console.log ('observable todolist : ' , this.todolists$);
+        console.log('observable todolist : ', this.todolists$);
         this.subscriptionTodolists$ = this.todolists$.subscribe(todolists => {
             console.log('SUBSCRIBE');
             this.ownerTodolist = todolists[0];
@@ -57,11 +68,12 @@ export class TodoslistPage implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         console.log("NG ON DESTROY");
-        console.log("closed : " , this.subscriptionTodolists$.closed);
+        console.log("closed : ", this.subscriptionTodolists$.closed);
         this.subscriptionTodolists$.unsubscribe();
     }
-/**
-    ionViewWillEnter(){
+
+    /**
+     ionViewWillEnter(){
         console.log("IONVIEWWILLENTER");
         this.currentUser = this.userService.get();
         console.log("Current user : " + JSON.stringify(this.currentUser));
@@ -85,13 +97,13 @@ export class TodoslistPage implements OnInit, OnDestroy {
             console.log('this.allowReadWriteTodolist : ', JSON.stringify(this.allowReadWriteTodolist));
         })
     }
- */
-/**
-    ionViewWillLeave(){
+     */
+    /**
+     ionViewWillLeave(){
         console.log("IONVIEWWILLLEAVE");
         this.subscription.unsubscribe();
     }
- */
+     */
 
     async displayPromptAddTodolist() {
         let alert = await this.alertCtrl.create({
@@ -137,7 +149,7 @@ export class TodoslistPage implements OnInit, OnDestroy {
         return todolist;
     }
 
-    delete(todolist: Todolist){
+    delete(todolist: Todolist) {
         this.listService.deleteTodolist(todolist);
     }
 
@@ -145,7 +157,44 @@ export class TodoslistPage implements OnInit, OnDestroy {
         return todolist.todos.every(todo => todo.isDone);
     }
 
-    countTodoDone(todolist: Todolist){
+    countTodoDone(todolist: Todolist) {
         return todolist.todos.filter(todo => todo.isDone).length;
+    }
+
+    buildInput(user: UserDB, todolist:Todolist) {
+        const isSharedReadWithUser = todolist.allowRead.filter(uid => uid === user.uid).length > 0;
+        return {
+            name:  user.name,
+            type: 'checkbox',
+            label: user.name,
+            value: user.uid,
+            checked: isSharedReadWithUser
+        }
+    }
+
+    async displayPromptShareTodolist(todolist:Todolist) {
+        console.log('hello world');
+        const input:any = this.users.map((user) => this.buildInput(user,todolist));
+
+        const alert = await this.alertCtrl.create({
+            header: 'Share with',
+            inputs: input,
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: () => {
+                        console.log('Confirm Cancel');
+                    }
+                }, {
+                    text: 'Ok',
+                    handler: () => {
+                        console.log('Confirm Ok');
+                    }
+                }
+            ]
+        });
+        await alert.present();
     }
 }
